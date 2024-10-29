a045282ef04947cd85e90507d993c19de08acac7
satvik-A <satvikaderla@gmail.com>
2024-10-29 19:36:41 +0530

    Update mainModel.java

--- 2d floor plan/lib/mainModel.java
+++ 2d floor plan/lib/mainModel.java
@@ -15,18 +15,37 @@ class mainModel {
 
         JPanel panelSIDE = new JPanel();
         panelSIDE.setBackground(Color.YELLOW);
-        panelSIDE.setPreferredSize(new Dimension(100, 100));
-        m.f.add(panelSIDE, BorderLayout.CENTER);
+        panelSIDE.setPreferredSize(new Dimension(150, 980));
+        panelSIDE.setLayout(new BorderLayout());
+        m.f.add(panelSIDE, BorderLayout.WEST);
 
         JPanel paneltop = new JPanel();
         paneltop.setBackground(Color.BLACK);
         paneltop.setPreferredSize(new Dimension(100, 100));
-        m.f.add(paneltop, BorderLayout.CENTER);
+        m.f.add(paneltop, BorderLayout.NORTH);
 
-        JPanel panelRight = new JPanel();
-        panelRight.setBackground(Color.BLUE);
-        panelRight.setPreferredSize(new Dimension(100, 100));
-        m.f.add(panelRight, BorderLayout.CENTER);
+        JPanel panelDB = new JPanel();
+        panelDB.setBackground(Color.BLUE);
+        panelDB.setPreferredSize(new Dimension(100, 100));
+        m.f.add(panelDB, BorderLayout.CENTER);
+
+        //
+        JPanel panelSIDE1 = new JPanel();
+        panelSIDE1.setBackground(Color.RED);
+        panelSIDE1.setPreferredSize(new Dimension(150, 100));
+        panelSIDE.add(panelSIDE1, BorderLayout.NORTH);
+        //
+
+        JPanel panelSIDE2 = new JPanel();
+        panelSIDE2.setBackground(Color.CYAN);
+        panelSIDE2.setPreferredSize(new Dimension(150, 100));
+        panelSIDE2.setLayout(new BorderLayout());
+        panelSIDE.add(panelSIDE2, BorderLayout.CENTER);
+
+        JPanel panelSIDE3 = new JPanel();
+        panelSIDE3.setBackground(Color.DARK_GRAY);
+        panelSIDE3.setPreferredSize(new Dimension(150, 100));
+        panelSIDE2.add(panelSIDE3, BorderLayout.NORTH);
 
     }
 
