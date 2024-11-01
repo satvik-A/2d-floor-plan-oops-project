da7f0e037a2be5e5695183969876ccb440d8fe10
satvik-A <satvikaderla@gmail.com>
2024-11-01 20:49:02 +0530

    Update mainModel.java

--- 2d floor plan/lib/mainModel.java
+++ 2d floor plan/lib/mainModel.java
@@ -13,17 +13,20 @@ class mainModel {
         m.f = new frame();
         m.f.setLayout(new BorderLayout());
 
+        // SIDE PANNEL / SIDEBAR
         JPanel panelSIDE = new JPanel();
         panelSIDE.setBackground(Color.YELLOW);
         panelSIDE.setPreferredSize(new Dimension(150, 980));
         panelSIDE.setLayout(new BorderLayout());
         m.f.add(panelSIDE, BorderLayout.WEST);
 
+        // INSTRUMENT PANNEL / TOPBAR /
         JPanel paneltop = new JPanel();
         paneltop.setBackground(Color.BLACK);
         paneltop.setPreferredSize(new Dimension(100, 100));
         m.f.add(paneltop, BorderLayout.NORTH);
 
+        // DRAWING BORD /
         JPanel panelDB = new JPanel();
         panelDB.setBackground(Color.BLUE);
         panelDB.setPreferredSize(new Dimension(100, 100));
@@ -35,8 +38,17 @@ class mainModel {
         panelSIDE1.setPreferredSize(new Dimension(150, 100));
         panelSIDE.add(panelSIDE1, BorderLayout.NORTH);
         //
-// inside action listener for side panal object creator like creation of new rooms
-        
+
+        //
+        JPanel panelSIDE4 = new JPanel();
+        panelSIDE4.setBackground(Color.RED);
+        panelSIDE4.setPreferredSize(new Dimension(150, 100));
+        panelSIDE.add(panelSIDE4, BorderLayout.SOUTH);
+        //
+
+        // inside action listener for side panal object creator like creation of new
+        // rooms
+
         JPanel panelSIDE2 = new JPanel();
         panelSIDE2.setBackground(Color.CYAN);
         panelSIDE2.setPreferredSize(new Dimension(150, 100));
